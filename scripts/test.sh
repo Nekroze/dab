c97105e351b65817cdfb20a3bc54f14ead15f4e5
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2018-10-07 13:08:58 +1000

    Use new tests orphan branch
    Isolates tests into a new branch
    
    Fixes #31

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,20 +2,11 @@
 # vim: ft=sh ts=4 sw=4 sts=4 noet
 set -eu
 
-# Attempt to clean test directory.
-if [ -d /tmp/dab/repos ]; then
-	sudo rm -rf /tmp/dab || true
+export DAB="$PWD"
+
+if [ ! -d tests ]; then
+	git clone -b tests https://github.com/Nekroze/dab.git tests
 fi
 
-# Ensure dab image is up to date.
-# shellcheck disable=SC1091
-. ./scripts/build.sh
-
-# Pull the last dab image for caching.
-doco pull test || true
-
-# run test container and pass any params to this script to cucumber.
-doco run --rm test "$@"
-
-# cleanup after a successful run.
-doco down --volumes
+cd ./tests
+./scripts/test.sh "$@"
