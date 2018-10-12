b4b7702b146327805560949872b77df3a96bcf41
Taylor Nekroze Lawson <taylorl@benon.com>
2018-10-12 15:23:32 +1000

    Remove dead code

--- app/subcommands/update.sh
+++ app/subcommands/update.sh
@@ -8,13 +8,5 @@ set -euf
 # shellcheck disable=SC1091
 . ./lib/update.sh
 
-case "${1:-}" in
-'-h' | '--help' | 'help')
-	inform 'The update subcommand will attempt to udpate the dab image.'
-	inform 'There are no SUBCOMMANDS.'
-	exit 0
-	;;
-esac
-
 export self_update_period=10
 maybe_selfupdate_dab
