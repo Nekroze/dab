3637b9a713c0fe7a8869e5778a8ac19edee02f4b
Taylor Nekroze Lawson <taylorl@benon.com>
2018-10-17 14:22:10 +1000

    Fix fetch
    Correctly takes no arguments and iterates over all repo's

--- app/subcommands/repo/fetch.sh
+++ app/subcommands/repo/fetch.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Description: Fetch all repos to check for available updates
-# Usage: <REPO_NAME>
 # vim: ft=sh ts=4 sw=4 sts=4 noet
 set -eu
 
@@ -9,9 +8,8 @@ set -eu
 # shellcheck disable=SC1091
 . ./lib/update.sh
 
-[ -n "${1:-}" ] || fatality 'must provide a repo name'
-
 set +f
 for repo in "$DAB_CONF_PATH/repo/"*; do
-	maybe_selfupdate_repo "$(echo "$repo" | sed "s/^$DAB_CONF_PATH//")"
+	name="$(basename "$repo" | sed "s@^$DAB_CONF_PATH@@")"
+	[ -d "$DAB_REPO_PATH/$name" ] && maybe_selfupdate_repo "$name"
 done
