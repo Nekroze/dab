ee6a1e8cce5e61775ee4d75ec8a3e635cd005447
Taylor "Nekroze" Lawson <nekroze.lawson@gmail.com>
2018-10-19 13:31:51 +1000

    Hook to add chronograf annotations for all executions (#109)

--- app/lib/hooks.sh
+++ app/lib/hooks.sh
@@ -9,7 +9,23 @@ set -euf
 # shellcheck disable=SC1091
 . ./lib/update.sh
 
+influx() {
+	servicepose run --rm influxdb influx -host services_influxdb_1 "$@"
+}
+
+maybe_post_chronograf_annotiation() {
+	if ! silently docker top services_influxdb_1; then
+		return 0
+	fi
+	ns="$(date +%s)000000000"
+	values="text=\"dab $1\",start_time=${ns}i,modified_time_ns=${ns}i,type=\"dab execution\",deleted=false"
+	influx -database chronograf -execute "INSERT annotations,id=$(uuidgen) $values" || influx -execute 'CREATE DATABASE chronograf'
+}
+
 hooks() {
+	quietly maybe_post_chronograf_annotiation "$*" || true &
+	trap 'wait $(jobs -p)' EXIT
+
 	config_load_envs || true
 	maybe_notify_wrapper_update || true
 	maybe_update_completion || true
