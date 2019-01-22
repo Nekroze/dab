ec840aa1b0e0e5eb88b1ff07239a368d8dd0d061
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2019-01-23 05:41:41 +1000

    Add some suggested docker-compose switches to the default entrypoint

--- app/default-entrypoint.sh
+++ app/default-entrypoint.sh
@@ -6,7 +6,7 @@ COMPOSE_FILE="docker-compose.yml:$(docker-compose-gen network --external lab)"
 export COMPOSE_FILE
 
 ## Suggestions:
-# docker-compose up
+# docker-compose up -d --build
 # make test
 # dab apps start redis
 # envsubst < ./kubernetes.yml | kubectl apply -f -
