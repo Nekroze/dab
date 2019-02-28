2ea5da12dcaf7a1f7a9bfcce9095ead9b4c82388
Taylor "Nekroze" Lawson <nekroze.lives@gmail.com>
2019-03-01 07:43:43 +1000

    Limit nssdb cert inject and removal with pki commands to known dirs

---
+++ app/lib/pki.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# vim: ft=sh ts=4 sw=4 sts=4 noet
+
+# shellcheck disable=SC1090
+. "$DAB/lib/output.sh"
+
+_find_nssdbs_under() {
+	for fname in cert8.db cert9.db; do
+		carelessly find "$1" -name "$fname" -type f
+	done
+}
+
+_find_nssdbs() {
+	_find_nssdbs_under ~/.pki
+	_find_nssdbs_under ~/.mozilla
+	_find_nssdbs_under ~/.config/browsh
+	_find_nssdbs_under ~/Library/Application\ Support/Firefox/Profiles
+}
+
+find_nssdbs() {
+	_find_nssdbs | sort -u
+}
