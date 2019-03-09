6fac401f263833192a2bfeffe5c3aea4934638a6
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2019-03-09 14:44:21 +1000

    Passthrough DAB_APPS_* env vars to their apps with the prefix removed

--- app/lib/docker.sh
+++ app/lib/docker.sh
@@ -6,14 +6,19 @@
 
 dpose_all() {
 	[ "${DAB_PROFILING:-false}" = 'false' ] || echo "[PROFILE] $(date '+%s.%N') [STRT] dpose_all $*"
+
+	app_envs_to_files_muxing
 	env COMPOSE_PROJECT_NAME=dab \
 		COMPOSE_FILE="$(find "$DAB/docker" -type f -name 'docker-compose.*.yml' | tr '\n' ':' | sed 's/:$//')" \
 		docker-compose --project-directory "$DAB/docker" "$@"
+
 	[ "${DAB_PROFILING:-false}" = 'false' ] || echo "[PROFILE] $(date '+%s.%N') [STOP] dpose_all $*"
 }
 
 dpose() {
 	[ "${DAB_PROFILING:-false}" = 'false' ] || echo "[PROFILE] $(date '+%s.%N') [STRT] dpose $*"
+
+	app_envs_to_files_muxing
 	app="$1"
 	file="$DAB/docker/docker-compose.$app.yml"
 	[ -f "$file" ] || fatality "$app is not a defined app"
@@ -21,6 +26,7 @@ dpose() {
 	env COMPOSE_PROJECT_NAME=dab \
 		COMPOSE_FILE="$(get_docker_compose_files_for_app "$app")" \
 		docker-compose --project-directory "$DAB/docker" "$@" 3>&1 1>&2 2>&3 | sed '/If you removed or renamed this service in your compose file, you can run this command with the --remove-orphans flag to clean it up/d'
+
 	[ "${DAB_PROFILING:-false}" = 'false' ] || echo "[PROFILE] $(date '+%s.%N') [STOP] dpose $*"
 }
 
@@ -89,4 +95,26 @@ await_container_healthy() {
 	if ! ishmael healthy --wait "$await_container_healthy_timeout" "$id"; then
 		fatality "$display did not become healthy within $await_container_healthy_timeout seconds"
 	fi
+}
+
+app_envs_to_files_muxing() {
+	for pair in $(env | grep -E '^DAB_APPS_'); do
+		key=$(echo "$pair" | cut -d = -f 1)
+		val=$(echo "$pair" | cut -d = -f 2)
+
+		app=$(echo "$key" | cut -d _ -f 3 | tr '[:upper:]' '[:lower:]')
+		[ -n "$app" ] || continue
+		newkey=$(echo "$key" | cut -d _ -f 4-)
+		echo "$newkey=$val" >>"/tmp/denvmux/$app.env"
+	done
+}
+
+ensure_app_envs() {
+	mkdir -p /tmp/denvmux
+
+	# shellcheck disable=SC2044
+	for dcf in $(find "$DAB/docker" -type f -name 'docker-compose.*.yml'); do
+		app=$(basename "$dcf" | cut -d . -f 2)
+		touch "/tmp/denvmux/$app.env"
+	done
 }
