d98c5108b7a5659fa8c656cdf9ba69868f2b43bc
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2019-03-18 18:34:30 +1000

    Faster local testing cycles by not rebuilding in the same script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -eu
 
 export COMPOSE_PROJECT_NAME='dab'
-export COMPOSE_FILE="tests/docker-compose.yml:tests/docker-compose.${TEST_DOCKER:-local}.yml"
+export COMPOSE_FILE="tests/docker-compose.yml:tests/docker-compose.${TEST_DOCKER:=local}.yml"
 
 # Build just the dab image and clean up.
 docker-compose build --force-rm --pull dab
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,8 @@
 # vim: ft=sh ts=4 sw=4 sts=4 noet
 set -eu
 
-export TEST_DOCKER="${TEST_DOCKER:-local}"
 export COMPOSE_PROJECT_NAME='dab'
-export COMPOSE_FILE="tests/docker-compose.yml:tests/docker-compose.$TEST_DOCKER.yml"
-trap 'docker-compose stop' EXIT
+export COMPOSE_FILE="tests/docker-compose.yml:tests/docker-compose.${TEST_DOCKER:=local}.yml"
 
 echo
 echo Docker Compose Config
@@ -13,22 +11,28 @@ echo
 docker-compose config
 echo
 
-if [ "$TEST_DOCKER" = 'local' ]; then
-	[ -d /tmp/dab ] && sudo rm -rf /tmp/dab
-	mkdir -p /tmp/dab/test_results
-fi
-
 # Cleanup first
 docker-compose down --volumes
 
 # Pull/build the latest test images.
 docker-compose pull tests || true
 
-# Start the docker in docker daemon, isolating it from the host.
-[ "$TEST_DOCKER" = 'local' ] || docker-compose up -d --remove-orphans docker
+# Ensure the mess we are about to make will be somewhat cleaned up
+trap 'docker-compose down' EXIT
 
-# run build container to get the image in dind.
-docker-compose run --rm build
+if [ "$TEST_DOCKER" = 'local' ]; then
+	# Cleanup previous test space
+	[ -d /tmp/dab ] && sudo rm -rf /tmp/dab
+elif [ "$TEST_DOCKER" = 'dind' ]; then
+	# run build container to get the image in dind.
+	docker-compose run --rm build
+
+	# Start the docker in docker daemon, isolating it from the host.
+	docker-compose up -d --remove-orphans docker
+fi
+
+# Create new test space
+mkdir -p /tmp/dab/test_results
 
 # run tests container and pass any params to this script to cucumber.
 docker-compose run tests --tags="${TEST_TAGS:-not @none}" --order=random "$@"
